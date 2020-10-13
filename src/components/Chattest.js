b9bb4201160cba907b1c33504f186632c3094c42
RebuildYY <dbstjrrl803@gmail.com>
2020-10-13 19:43:27 +0900

    useEffect for only one render in chatting system

--- src/components/Chattest.js
+++ src/components/Chattest.js
@@ -1,28 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import SocketIOClient from 'socket.io-client';
+import store from '../store';
 
-class Chattest extends Component {
-    constructor(props){
-        super(props);
-        this.socket = SocketIOClient('http://localhost:3001');
-    };
-
-  render() {
-    this.socket.on('chat message', (msg)=>{
-        console.log(msg);
+function Chattest(){
+  var socket = SocketIOClient('http://localhost:3001');
+  var message=''
+  useEffect(()=>{ 
+    socket.on('chat message', (msg)=>{
+      console.log(msg);
+      message = msg;
     });
+  },[]);//빈 배열을 줌으로써 최초1번만 렌더링 되게 한다. 
+  return (
+      <div className="Chatting">
+          <form id='chat' onSubmit={(e)=>{
+            e.preventDefault();
+            console.log("in form");
+            socket.emit('chat message', e.target.m.value);
+          }}>
+          <input id="m" autoComplete="off" /><button>Send</button>
+        <div>{message}</div>
+      </form>
+    </div>
+  );
+}
+
+/*
+class Chattest extends Component {
+  socket = SocketIOClient('http://localhost:3001');
+  state = {messages:store.getState().messages}
+  constructor(props){
+    super(props);
+  }
+    
+  render() {
+    setMsg(this.socket);
     return (
       <div className="Chatting">
           <form id='chat' onSubmit={(e)=>{
               e.preventDefault();
+              console.log("in form");
               this.socket.emit('chat message', e.target.m.value);
           }}>
-            <input id="m" autocomplete="off" /><button>Send</button>
-
+            <input id="m" autoComplete="off" /><button>Send</button>
+          <div>{this.state.message}</div>
           </form>
       </div>
     );
   }
 }
-
+*/
 export default Chattest;

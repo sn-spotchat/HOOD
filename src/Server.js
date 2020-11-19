8d5e3ce7fca08571d232577355155aa598984644
LeeYunSeok <41578156+RebuildYY@users.noreply.github.com>
2020-11-20 00:55:52 +0900

    server.js test.js에 맞게 변수명 수정

--- src/Server.js
+++ src/Server.js
@@ -1,19 +1,3 @@
-
-const firebase = require('react-firebase');
-const firebaseConfig = {
-  apiKey: "AIzaSyDNe_G-z5EPOI7-t7xjEHhA7Fx8yRGXS74",
-  authDomain: "hood-sgtmi.firebaseapp.com",
-  databaseURL: "https://hood-sgtmi.firebaseio.com",
-  projectId: "hood-sgtmi",
-  storageBucket: "hood-sgtmi.appspot.com",
-  messagingSenderId: "80145957779",
-  appId: "1:80145957779:web:e72842d0c7605211795e66",
-  measurementId: "G-TPHYH6LN5T"
-};
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.database();
-
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);

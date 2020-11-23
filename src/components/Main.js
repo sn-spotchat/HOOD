a4453c36d8bf5ae0d3b8f3f8c88031a0db1fb12d
LeeYunSeok <41578156+RebuildYY@users.noreply.github.com>
2020-11-23 17:40:41 +0900

    마우스 오버효과 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Navigation from './Navigation';
 import Map from './Map';
 import './Main.css';
 import Sidebar from './Sidebar';
 import SidebarContainer, { SidebarButton } from '../containers/SidebarContainer';
 const Main = () => {
-  
+  const sidebarstate = useSelector(state => state.reducer.sidebarstate, []);
   return (
     <div className='wrap'>
-      <Navigation/>
+      <Navigation sidebarstate={sidebarstate}/>
       <SidebarContainer/>
       <Map/>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,32 +19,39 @@ import PinDrop from '@material-ui/icons/PinDrop';
 추가해야 할 사항:
 클릭시 색깔이 변하여 현재 내가 무슨 작업을 하고 있는지 보여주게 한다.
 */
-const Navigation = () => {
+const Navigation = ({sidebarstate}) => {
     const [NavList, setNavList] = useState([]);
+    const [NavState, setNavState] = useState("");
     const store_loggedin = useSelector(state => state.profilereducer.loggedin, []);
     const dispatch = useDispatch();
     const initialState = useSelector(state => state.mapreducer)
     console.log(initialState)
-    
-
+    console.log(sidebarstate)
+    console.log(initialState.sidebarstate)
     //the navigation Icons depend on 'bool loggedin' in store.
     useEffect(() => {
         setNavList([]);
+        setNavState(initialState.sidebarstate);
         if (store_loggedin == false) {
-            setNavList(oldList => [...oldList, { id: "Login", func: () => dispatch(actionType.sidebarloginObject), icon: <Person></Person>}]);
-            setNavList(oldList => [...oldList, { id: "Home", func: () => dispatch(actionType.sidebarhomeObject), icon: <Home></Home>}]);
+            setNavList(oldList => [...oldList, { id: "login", func: () => dispatch(actionType.sidebarloginObject), icon: <Person></Person>}]);
+            setNavList(oldList => [...oldList, { id: "home", func: () => dispatch(actionType.sidebarhomeObject), icon: <Home></Home>}]);
         }
         else {
-            setNavList(oldList => [...oldList, { id: "Mypage", func: () => dispatch(actionType.sidebarmypageObject), icon:<AccountCircle></AccountCircle> }]);
-            setNavList(oldList => [...oldList, { id: "Home", func: () => dispatch(actionType.sidebarhomeObject), icon: <Home></Home>}]);
-            setNavList(oldList => [...oldList, { id: "Near", func: () => dispatch(actionType.sidebarnearObject), icon: <PinDrop></PinDrop>}]);
-            setNavList(oldList => [...oldList, { id: "Chat", func: () => dispatch(actionType.sidebarchatObject), icon: <Forum></Forum>}]);
+            setNavList(oldList => [...oldList, { id: "mypage", func: () => dispatch(actionType.sidebarmypageObject), icon:<AccountCircle></AccountCircle> }]);
+            setNavList(oldList => [...oldList, { id: "home", func: () => dispatch(actionType.sidebarhomeObject), icon: <Home></Home>}]);
+            setNavList(oldList => [...oldList, { id: "near", func: () => dispatch(actionType.sidebarnearObject), icon: <PinDrop></PinDrop>}]);
+            setNavList(oldList => [...oldList, { id: "chat", func: () => dispatch(actionType.sidebarchatObject), icon: <Forum></Forum>}]);
         }
-    }, [store_loggedin]);
+    }, [store_loggedin, initialState]);
 
-
-
-
+    function navigation_present(element, index){//현재 sidebarstate에 따라 색깔을 달리 표시
+        if(element.id === NavState){
+            return <div id={element.id} className="NavigationIcon" onClick={element.func} key={index} style={{color:'white', backgroundColor:'blue'}}>{element.icon}</div>
+        }
+        else{
+            return <div id={element.id} className="NavigationIcon" onClick={element.func} key={index}>{element.icon}</div>
+        }
+    }
 
     return (
         <div className="Navigation">
@@ -58,3 +65,5 @@ const Navigation = () => {
 };
 
 export default Navigation;
+//<div id={element.id} className="NavigationIcon" onClick={element.func} key={index}>{element.icon}</div>
+//navigation_present(element, index)

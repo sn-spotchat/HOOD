483c0d6d2ef4a2af09fd9c5827496a3fdb654f16
LeeYunSeok <41578156+RebuildYY@users.noreply.github.com>
2020-11-26 17:30:42 +0900

    HomeEventBox 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useSelector} from 'react-redux';
+import HomeEventBox from './HomeEventBox';
 import './Home.css';
+import 'firebase/database';
+import {database, storage} from '../firebase.js';
 
 
 const Home = () =>{
+    const [EventList, setEventList] = useState([]);
     const user = useSelector(state => state.userreducer.profile.name); 
-
+    var storageRef = storage.ref();
+    useEffect(()=>{
+      database.ref("event").once('value', function(snapshot){
+        Object.values(snapshot.val()).forEach(value=>{
+          storageRef.child(value.imagesrc).getDownloadURL().then(function(url) {
+            setEventList(oldList => [...oldList, {
+            img: url,
+            content: value.content,
+            date: value.date,
+            name: value.name,
+            url: value.url
+          }]);
+          }).catch(function(error) {
+            console.log(error);
+          });
+          
+        });
+        
+      });
+    },[])
+    
     return (
         <div>
             <div id="homehead" className="head">Home</div>
             <div id="homebody" className="body">
-                {JSON.stringify(user)}
+              {EventList.map((element, index) => {
+                  return (
+                    <HomeEventBox  key={index} img={element.img} content={element.content} date={element.date} name={element.name} url={element.url}></HomeEventBox>
+                  )
+              })}
             </div>
         </div>
     );

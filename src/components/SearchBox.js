38e1a03be07913c2fda198018e38998bd815acbc
LeeYunSeok <41578156+RebuildYY@users.noreply.github.com>
2020-12-02 14:08:06 +0900

    좌표 변환

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as actionType from '../modules/action';
 import Button from '@material-ui/core/Button';
+import proj4 from 'proj4';
 import './SearchBox.css';
 
 const SearchBox = (props) => {
     const dispatch = useDispatch();
+    proj4.defs('TM128','+proj=tmerc +lat_0=38 +lon_0=128 +k=0.9999 +x_0=400000 +y_0=600000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43');
+    proj4.defs('EPSG:5179','+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=400000 +y_0=600000 +ellps=GRS80 +units=m +no_defs');
+    proj4.defs('WGS84', "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
+    
+    var p = proj4('TM128', 'WGS84');
     function setMarker(event){
-        dispatch(actionType.setMarkerX(props.mapx));
-        dispatch(actionType.setMarkerY(props.mapy));
+        var temp = p.forward([Number(props.mapx), Number(props.mapy)]);
+        console.log(temp);
+        dispatch(actionType.setMarkerX(temp[0]));
+        dispatch(actionType.setMarkerY(temp[1]));
     };
     function sendMarker(event){
         dispatch(actionType.setSidebar("chat"));
